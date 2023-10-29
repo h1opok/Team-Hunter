b8bd8b64db8175c90b966bb820813b7fd245db4c
Mizogg <88630056+Mizogg@users.noreply.github.com>
2023-10-29 16:29:02 +0000

    Add files via upload

---
+++ pip install.bat
@@ -0,0 +1,12 @@
+python -m pip install --upgrade pip
+pip install PyQt6
+pip install pyqtdarktheme
+pip install requests
+pip install base58
+pip install ecdsa
+pip install PyQt6-WebEngine
+pip install simplebloomfilter
+pip install bitarray==1.9.2
+pip install hdwallet
+pip install mnemonic
+pause
